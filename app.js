1be007e2efda2a6d877909a65ea0d0f190ac50b2
Scott D'Alessandro <sdaless22@gmail.com>
2016-11-05 13:29:27 -0400

    initial boilerplate w/ foundation

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(3001, function(){
+  console.log('server starting localhost:3001');
+});
+
+
+
+
+
