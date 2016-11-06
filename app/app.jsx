a647f03e52055db76240a1f93fde28ab91633d0c
Scott D'Alessandro <sdaless22@gmail.com>
2016-11-05 21:37:15 -0400

    add todoapp component

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var TodoApp = require('TodoApp');
 
 
 
@@ -11,7 +12,7 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p> Boilerplate 3 Project </p>,
+  <TodoApp />,
   document.getElementById('app')
 );
   
