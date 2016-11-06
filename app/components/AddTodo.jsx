a97c972a47c003023471a29d5175dce5461db8d0
Scott D'Alessandro <sdaless22@gmail.com>
2016-11-06 10:36:52 -0500

    created addTodo component and test suite

---
+++ app/components/AddTodo.jsx
@@ -0,0 +1,31 @@
+var React= require('react');
+
+var AddTodo = React.createClass({
+  handleSubmit: function(e){
+    e.preventDefault();
+    
+    var todoText = this.refs.todoText.value;
+    
+    if(todoText.length > 0){
+      this.refs.todoText.value = "";
+      this.props.handleAddTodo(todoText);
+    } else {
+      this.refs.todoText.focus(); // places cursor back into input field.
+    }
+      
+
+    
+  },
+  render: function(){
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input ref="todoText" type="text" placeholder="What do I need to do?"/>
+          <button className='button expanded'>Add Todo</button>          
+        </form>      
+      </div>
+    )
+  }
+});
+
+module.exports = AddTodo;
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
   getInitialState: function(){
@@ -7,11 +8,15 @@ var TodoApp = React.createClass({
       todos: [{id: 1, text: "Finish TodoApp"}, {id:2, text:'Finish Wedding Invites'}]
     }
   },
+  handleAddTodo: function(text) {
+    alert('new todo: ' + text);
+  },
   render: function(){
     var {todos} = this.state;
     return (
       <div>
         <TodoList todos={todos} />
+        <AddTodo handleAddTodo={this.handleAddTodo}/>
       </div>
     )
   }
