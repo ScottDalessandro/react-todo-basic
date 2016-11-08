a4439ced6ca735130d2b6a5f827177c117ecbda0
Scott D'Alessandro <sdaless22@gmail.com>
2016-11-07 22:07:32 -0500

    Add static search components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,20 +1,30 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState: function(){
     return {
+      showCompleted: false,
+      searchText: "",
       todos: [{id: 1, text: "Finish TodoApp"}, {id:2, text:'Finish Wedding Invites'}]
     }
   },
   handleAddTodo: function(text) {
     alert('new todo: ' + text);
   },
+  handleSearch: function(showCompleted, searchText){
+    this.setStat({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   render: function(){
     var {todos} = this.state;
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch} />
         <TodoList todos={todos} />
         <AddTodo handleAddTodo={this.handleAddTodo}/>
       </div>
